7aba2c260117ee98351f655e08992b6a1b6a85d3
Jun Wang <junwang@Juns-MacBook-Pro.local>
2017-02-28 10:53:47 -0800

    state and action flow added.

--- thinking-in-react/src/FilterableProductTable.js
+++ thinking-in-react/src/FilterableProductTable.js
@@ -15,15 +15,44 @@ const user = {
 };
 
 class FilterableProductTable extends Component {
-  render() {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+            inStockOnly: false
+        };
+
+        this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
+        this.handleInStockInput = this.handleInStockInput.bind(this);
+    }
+
+    handleFilterTextInput(filterText) {
+        this.setState({
+            filterText: filterText
+        });
+    }
+
+    handleInStockInput(inStockOnly) {
+        this.setState({
+            inStockOnly: inStockOnly
+        })
+    }
+
+    render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React {formatName(user)}</h2>
         </div>
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar filterText={this.state.filterText}
+                   inStockOnly={this.state.inStockOnly}
+                   onFilterTextInput={this.handleFilterTextInput}
+                   onInStockInput={this.handleInStockInput}
+        />
+        <ProductTable products={this.props.products} filterText={this.state.filterText}
+                      inStockOnly={this.state.inStockOnly}/>
       </div>
     );
   }
--- thinking-in-react/src/ProductTable.js
+++ thinking-in-react/src/ProductTable.js
@@ -7,8 +7,12 @@ class ProductTable extends Component {
     render() {
         var rows = [];
         var lastCategory = null;
+        var that = this;
 
         this.props.products.forEach(function(product) {
+            if (product.name.indexOf(that.props.filterText) === -1 || (!product.stocked && that.props.inStockOnly)) {
+                return;
+            }
             if (product.category !== lastCategory) {
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
             }
